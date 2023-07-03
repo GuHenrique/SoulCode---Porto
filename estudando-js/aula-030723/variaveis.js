c53d41691ed27869f81725e5ccb0f72cdd639a9c
Gustavo <gustavo27hs@gmail.com>
2023-07-03 12:02:52 -0300

    Início dos estudos em JavaScript - variáveis

---
+++ estudando-js/aula-030723/variaveis.js
@@ -0,0 +1,18 @@
+/* 
+  Chrome -> V8 - Interpretador
+  Node.js -> V8 + NPM (Node Package Manager)
+*/
+
+// Variáveis
+
+let num = 1;
+let num2 = 2.4
+let texto = "dassda";
+let booleano = true;
+
+// Tipagem dinâmica
+
+console.log(num + " é um(a) " + typeof (num));
+console.log(num2 + " é um(a) " + typeof (num2));
+console.log(texto + " é um(a) " + typeof (texto));
+console.log(booleano + " é um(a) " + typeof (booleano));
