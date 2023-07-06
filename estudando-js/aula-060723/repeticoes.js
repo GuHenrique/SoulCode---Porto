3b91397d24bc411a5a018504df6e895171e06370
Gustavo <gustavo27hs@gmail.com>
2023-07-06 13:06:35 -0300

    Estruturas de repetição - JS

---
+++ estudando-js/aula-060723/repeticoes.js
@@ -0,0 +1,15 @@
+// Estruturas de repetições
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i + " x 6 = " + i * 6);
+
+}
+
+let numeroAleatorio = Math.floor(Math.random() * 100 + 1);
+console.log(numeroAleatorio)
+
+let n = 0;
+while (n < 18) {
+  console.log("SoulCode + PortoSeguro")
+  n++;
+}
