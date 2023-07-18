87694572b0bc807eb5fa07a4aed9ec7c352e1c05
Gustavo <gustavo27hs@gmail.com>
2023-07-18 11:30:50 -0300

    Acessando e manipulando elementos com DOM - JS

---
+++ estudando-js/aula-180723/galeria-imagens/script.js
@@ -0,0 +1,25 @@
+const imagens = [
+  "https://images.unsplash.com/photo-1689101298752-886c970ff200?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+  "https://images.unsplash.com/photo-1688670564879-239883a0e330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=663&q=80",
+  "https://images.unsplash.com/photo-1683404633769-a33c656df6a8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+  "https://images.unsplash.com/photo-1688376672114-9f3e0861e195?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80",
+  "https://images.unsplash.com/photo-1682188299490-1e6e9c98bac8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=690&q=80"
+]
+
+
+let imagem = document.querySelector("#galeria");
+let indiceAtual = 1;
+
+
+function galeria() {
+  imagem.src = imagens[indiceAtual];
+  if (indiceAtual == (imagens.length - 1)) {
+    indiceAtual = 0;
+  } else {
+    indiceAtual++;
+  }
+}
+
+
+setInterval(galeria, 2000);
+
---
+++ estudando-js/aula-180723/script.js
@@ -0,0 +1,41 @@
+// window.document -> Objeto que representa o documento HTML
+
+// Busca pelo Id do elemento;
+let titulo = document.getElementById("titulo");
+
+// Busca todos os elementos por class;
+let paragrafos = document.getElementsByClassName("paragrafo");
+
+// Busca todos os elementos pela tag HTML;
+let negritos = document.getElementsByTagName("b");
+
+// Busca UM elemento com base no seu seletor CSS;
+let inputEmail = document.querySelector("input[type=email]");
+let negritoParagrafo = document.querySelector("p b");
+let paragrafo = document.querySelector(".paragrafo");
+
+// Busca TODOS elemento com base no seu seletor CSS;
+let negritosParagrafos = document.querySelectorAll("p b");
+
+
+
+
+// Alinha o elemento;
+titulo.align = "center";
+// Mensagem que é exibida quando o mouse fica em cima do elemento;
+titulo.title = "Titulo principal"
+// Acessa o objeto style e dentro dela acessa a propriedade color;
+titulo.style.color = "blue";
+// Acessa o objeto style e dentro dela acessa a propriedade fontSize;
+titulo.style.fontSize = "80px"
+// Traz o conteúdo textual do elemento;
+console.log(paragrafo.innerText);
+// Traz o conteudo HTML do elemento;
+console.log(paragrafo.innerHTML);
+// substitui o conteudo textual e HTML, caso queira inserir uma tag html é possível;
+paragrafo.innerHTML = "Testando o atributo <b>innerHTML</b>";
+// substitui o conteudo textual, caso queira inserir uma tag html NÃO é possível;
+paragrafo.innerText = "Testando o atributo <b>innerHTML</b>";
+titulo.innerText = "RONALDO FENOMENO";
+// Se setado para true, esconde o elemento.
+paragrafos[0].hidden = false;
