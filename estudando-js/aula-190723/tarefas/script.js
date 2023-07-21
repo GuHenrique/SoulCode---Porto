91494410e000d4c80f963c6c1e9ccbee9229bc65
Gustavo <gustavo27hs@gmail.com>
2023-07-21 11:25:28 -0300

    Correção e finalização da atividade de criar um TODO

--- estudando-js/aula-190723/tarefas/script.js
+++ estudando-js/aula-190723/tarefas/script.js
@@ -1,79 +1,96 @@
-const tarefas = [];
+const tarefas = [{
+	descricao: "teste",
+	prioriade: "teste",
+	status: "teste",
+	prazo: new Date()
+}];
 
 function gerarTabela() {
-  let tabela = `
-    <table>
-      <tr>
-        <th>Descrição</th>
-        <th>Prioridade</th>
-        <th>Status</th>
-        <th>Prazo</th>
-        <th>Remover / Concluir</th>
-        
-      </tr>
+	let tabela = `
+	
+	<table>
+		<tr>
+
+			<th>Concluir</th>
+			<th>Descrição</th>
+			<th>Prioridade</th>
+			<th>Status</th>
+			<th>Prazo</th>
+			<th>Remover</th>
+
+	  	</tr>
+
   `;
 
-  tarefas.forEach((tarefa, index) => {
-    tabela += `
 
-      <tr>
-        <td>${tarefa.descricao}</td>
-        <td>${tarefa.prioridade}</td>
-        <td>${tarefa.stat}</td>
-        <td>${tarefa.prazo}</td>
-        <td>
-          <button class="btnRemove" data-index="${index}"> Remover </button> 
-          <button class="btnDone" data-index="${index}"> Concluir </button> 
-        </td>
-      </tr>
-    `
-  })
+	tarefas.forEach((tarefa, index) => {
+		tabela += `
 
-  tabela += '</table>';
+			<tr>
+				<td>
+					<input type="checkbox" class="btnDone" 
+					${tarefa.status == "Tarefa concluida" && "checked"} 
+					data-index="${index}">
+				</td>
+			
+				<td>${tarefa.descricao}</td>
+				<td>${tarefa.prioridade}</td>
+				<td>${tarefa.status}</td>
+				<td>${tarefa.prazo}</td>
+				<td>
+					<button class="btnRemove" data-index="${index}"> Remover </button>
+				</td>
+			</tr>
+		`
+	})
 
-  document.querySelector("#container").innerHTML = tabela;
+	tabela += '</table>';
 
-  const btnRemove = document.querySelectorAll(".btnRemove");
-  for (let i = 0; i < btnRemove.length; i++) {
-    btnRemove[i].addEventListener("click", removerTarefa);
-  }
+	document.querySelector("#container").innerHTML = tabela;
 
-  const btnDone = document.querySelectorAll(".btnDone");
-  for (let i = 0; i < btnRemove.length; i++) {
-    btnDone[i].addEventListener("click", concluirTarefa);
-  }
+	const btnRemove = document.querySelectorAll(".btnRemove");
+	for (let i = 0; i < btnRemove.length; i++) {
+		btnRemove[i].addEventListener("click", removerTarefa);
+	}
+
+	const btnDone = document.querySelectorAll(".btnDone");
+	for (let i = 0; i < btnRemove.length; i++) {
+		btnDone[i].addEventListener("click", concluirTarefa);
+	}
 
 }
 
 
 document.querySelector("#formTarefa").addEventListener("submit", e => {
-  e.preventDefault();
 
-  const descricao = document.querySelector("#descricao").value;
-  const prioridade = document.querySelector("#prioridade").value;
-  const stat = document.querySelector("#status").value;
-  const prazo = document.querySelector("#prazo").value;
+	e.preventDefault();
 
-  tarefas.push({ descricao, prioridade, stat, prazo });
+	const descricao = document.querySelector("#descricao").value;
+	const prioridade = document.querySelector("#prioridade").value;
+	const status = document.querySelector("#status").value;
+	const prazo = document.querySelector("#prazo").value;
 
-  document.querySelector("#formTarefa").reset();
+	tarefas.push({ descricao, prioridade, status, prazo });
 
-  gerarTabela();
+	document.querySelector("#formTarefa").reset();
 
+	gerarTabela();
 
-})
-
-
+});
 
 
 function removerTarefa() {
-  const index = this.getAttribute("data-index");
-  tarefas.splice(index, 1);
-  gerarTabela();
-}
+	const index = this.getAttribute("data-index");
+	tarefas.splice(index, 1);
+	gerarTabela();
+};
 
 function concluirTarefa() {
-  const index = this.getAttribute("data-index");
-  tarefas[index].stat = "Tarefa concluida";
-  gerarTabela();
-}
+	const index = this.getAttribute("data-index");
+	tarefas[index].status = "Tarefa concluida";
+	gerarTabela();
+
+
+};
+
+gerarTabela();
